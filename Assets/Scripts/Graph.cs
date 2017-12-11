f3dc03b526aabf7dbe152e814746ced4fbb3f6de
Estevan Gladstone <estevangladstone@ejcm.com.br>
2017-12-11 21:19:52 -0200

    First commit

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,86 @@
+﻿//using System;
+//
+//namespace AssemblyCSharp
+//{
+//	public class EmptyClass
+//	{
+//		public EmptyClass ()
+//		{
+//		}
+//	}
+//}
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph {
+
+	private Dictionary<GameObject, GameObject[]> vertices = new Dictionary<GameObject, GameObject[]>();
+	private List<GameObject> path = null;
+	private int currentVertexIndex = null;
+
+	public void AddVertex(GameObject vertex, GameObject[] neighbors)
+	{
+		vertices[vertex] = neighbors;
+	}
+
+	public List<GameObject> shortest_path(GameObject start, GameObject finish) // Recebe o ROBO e o GOAL
+	{
+		var previous = new Dictionary<GameObject, GameObject>();
+		var distances = new Dictionary<GameObject, float>();
+		var nodes = new List<GameObject>();
+
+		foreach (var vertex in vertices) {
+			if (vertex == start) {
+				distances[vertex] = 0; 
+			} else {
+				distances[vertex] = float.MaxValue; 
+			}
+
+			nodes.Add(vertex);
+		}
+
+		while (nodes.Count != 0) {
+			nodes.Sort((x, y) => distances[x] - distances[y]);
+
+			var smallest = nodes[0];
+			nodes.Remove(smallest);
+
+			if (smallest == finish) {
+				path = new List<GameObject>();
+				while (previous.ContainsKey(smallest)) {
+					path.Add(smallest);
+					smallest = previous[smallest];
+				}
+
+				break;
+			}
+
+			if (distances[smallest] == float.MaxValue) {
+				break;
+			}
+
+			foreach (var neighbor in vertices[smallest]) {
+				var alt = distances[smallest] + Vector3.Distance(neighbor.transform.postion, vertices[smallest].transform.postion);
+				if (alt < distances[neighbor]) {
+					distances[neighbor] = alt;
+					previous[neighbor] = smallest;
+				}
+			}
+		}
+
+		return path;
+	}
+
+	public Vector3 nextVertexPosition ()
+	{
+		if(currentVertexIndex == null) {
+			currentVertexIndex = 0;
+		} else {
+			currentVertexIndex = currentVertexIndex + 1;
+		}
+		return path[currentVertexIndex].transform.position;
+	}
+
+}
---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	private Graph graph;
+	private Vector3 target;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void WalkPath ()
+	{
+		if(transform.position == target) {
+			target = graph.nextVertexPosition();
+		}
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, step);
+	}
+
+	void createGraph ()
+	{
+		graph = new Graph();
+//		foreach (GameObject sensor in sensors){}
+	}
+
+}
